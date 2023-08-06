bbce6455cc3a5bba96f7c1a104ac10d4442962c0
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-08-06 06:57:28 +0400

    Registeyed transfer model to django admin panel

--- backend/transfer/admin.py
+++ backend/transfer/admin.py
@@ -4,7 +4,7 @@ from .models import *
 
 
 
-#!ApproveModelAdmin
+#!TransferModelAdmin
 class TransferModelAdmin(admin.ModelAdmin):
     list_display = ['transfer_from','transfer_to','transfer_hash','transfer_amount','is_transfer','confirmations','created','modified','transfer_slug']
     list_display_links = ['transfer_from','transfer_to','transfer_hash']
