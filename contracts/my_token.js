62cc779befc1e31bb500dd533e03e269422e2361
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-08-06 07:02:24 +0400

    Is updating deploytoken method

--- contracts/my_token.js
+++ contracts/my_token.js
@@ -11,41 +11,6 @@ const MyTokenProd = require('../data/MyTokenProd.json')
 require('dotenv').config({path:"..//.env"})
 
 
-//*deployContract Prod
-// const deployContract = async () => {
-//     let contract;
-//     let deployeNetwork=null
-//     let userBalance = null
-//     let signer = null
-
-//     if(typeof window !== 'undefined' && typeof window.ethereum !== 'undefined'){
-//         //We are in the browser and metamask is running
-//         const accounts = window.ethereum.request({method:'eth_requestAccounts'})
-//         if(await accounts){
-//             console.log('Worked if and our contract deploy on sepolia')
-//             const provider = new Ethers.providers.Web3Provider(window.ethereum)
-//             signer = provider.getSigner()
-//             contract = new Ethers.Contract(MyTokenProd.address,MyTokenProd.abi, signer)
-//             userBalance = (await signer.getBalance()).toString()
-//             deployeNetwork=MyTokenProd.network
-//         }
-//         else{
-//             alert('Please login to metamask account')
-//         }
-//     }
-//     else{
-//         console.log('Worked else and our contract deploy on localhost')
-//         signer = ethers.getSigner()
-//         contract = new Ethers.Contract(MyTokenLocal.address,MyTokenLocal.abi,signer)
-//         let ub =  await contract.balanceOf((await ethers.getSigner().getAddress()))
-//         userBalance = ub.toString()
-//         deployeNetwork=MyTokenLocal.network
-//     }
-//     return {contract,deployeNetwork,userBalance,signer}
-// }
-// deployContract()
-
-
 
 //*deployContract Local Hardhat
 const deployContract = async () => {
