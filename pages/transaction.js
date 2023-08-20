4f982802fedf704f75664bcc64bb3c87f58977f7
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-08-20 09:42:53 +0400

    Solving some bugs

--- pages/transaction.js
+++ pages/transaction.js
@@ -28,6 +28,10 @@ import ReactPaginate from 'react-paginate';
 import Switch from '@mui/material/Switch';
 import FormControlLabel from '@mui/material/FormControlLabel';
 
+
+
+
+
 //?ContractTransition
 export default function ContractTransition(){
 
@@ -56,10 +60,12 @@ export default function ContractTransition(){
 
 
     //pagination
-    let itemsPerPage = 10
+    let itemsPerPage = 2
     const endOffset = itemOffset + itemsPerPage;
-    const currentItems = itemsOwnerToUser.slice(itemOffset, endOffset)
-    const pageCount = Math.ceil(itemsOwnerToUser.length / itemsPerPage);
+    const currentItems = userTransactionAndTransfer.slice(itemOffset, endOffset)
+    const pageCount = Math.ceil(userTransactionAndTransfer.length / itemsPerPage);
+
+    console.log('Page count is ', pageCount);
 
 
 
@@ -77,16 +83,17 @@ export default function ContractTransition(){
     const getUserTransactionAndTransferData = async (api_url) => {
         console.log('Qaqasss bunediii ', api_url);
         setUserTransactionAndTransfer([])
-        axios.get(api_url)
+        await axios.get(api_url)
             .then((response) => {
                 console.log('Qaqa response geldiee ', response.data)
                 console.log('Qaqa response uzunlug ', response.data.length)
                 setUserTransactionAndTransfer(response.data)
+                return 
             })
             .catch((err)=>{
                 console.log('Ala errorrr ', err)
             })
-    
+            
     }
 
 
@@ -104,30 +111,37 @@ export default function ContractTransition(){
         }
 
         
-        if(!loading && server_name=='production'){
+        if(server_name=='production'){
+            // http://127.0.0.1:8000/transaction/get/0xa2f1e30Af99632d6DbC7EBf8b8B9Be5D5Ca13358/production/
+            // http://127.0.0.1:8000/transfer/get/0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65/local/
+
             // http://127.0.0.1:8000/transaction/get/0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266/
             // http://127.0.0.1:8000/transfer/get/0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65/
-            getUserTransactionAndTransferData(`http://127.0.0.1:8000/transaction/get/${currentUserAddress}/`) // 10
+            getUserTransactionAndTransferData(`http://127.0.0.1:8000/transaction/get/${currentUserAddress}/${server_name}/`) // 10
             console.log('Ay qaaa production')
+            return
         }
         else if(!loading && server_name=='local' && transaction_type==null){
-            getUserTransactionAndTransferData(`http://127.0.0.1:8000/transaction/get/${currentUserAddress}/`) // 200
-            console.log('Ay broo LOCAL Transaction')            
+            getUserTransactionAndTransferData(`http://127.0.0.1:8000/transaction/get/${currentUserAddress}/${server_name}/`) // 200
+            console.log('Ay broo LOCAL Transaction')    
+            return        
         }
         else if(transaction_type != 'Transfer'){
-            getUserTransactionAndTransferData(`http://127.0.0.1:8000/transfer/get/${currentUserAddress}/`) //100
+            getUserTransactionAndTransferData(`http://127.0.0.1:8000/transfer/get/${currentUserAddress}/${server_name}/`) //100
             console.log('Demeli bura transferdi')
+            return
         }
         else{
-            getUserTransactionAndTransferData(`http://127.0.0.1:8000/transaction/get/${currentUserAddress}/`) // 200
+            getUserTransactionAndTransferData(`http://127.0.0.1:8000/transaction/get/${currentUserAddress}/${server_name}/`) // 200
             console.log('Demeli bura transactiondu')
+            return
         }
     }
 
 
        // Invoke when user click to request another page.
     const handlePageClick = (event) => {
-        const newOffset = (event.selected * itemsPerPage) % itemsOwnerToUser.length;
+        const newOffset = (event.selected * itemsPerPage) % userTransactionAndTransfer.length;
         setItemOffset(newOffset);
     };
 
@@ -169,75 +183,156 @@ export default function ContractTransition(){
         getContractInformation()
     },[])
 
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+
+
+
     return(
         <>
             <div className='container'>
                     <div className="m-5">
                         <Header/>
                     </div>
-                    <div className="bg-light text-left p-5 m-5 shadow">
-
-                    <FormControlLabel
-                        sx={{display: 'block'}}
-                        control={
-                            <Switch checked={loading} onChange={() =>getTransactionAndTransferData(serverName,document.getElementById('transaction_text').textContent) && setLoading(!loading)} name="loading" color="primary" disabled={loading && serverName=='production' ? true : false}/>
-                        }
-                        label={<span style={{fontWeight: 'bold',color:'black'}} id='transaction_text'>{loading && serverName=='production' ? "Transactions" : (!loading && serverName=='local' ? 'Transactions' : 'Transfer' )}</span>}
-                    />
-                    <hr/>
+            </div>
 
 
-                    Alaaa : {userTransactionAndTransfer.length}
-                    Current Account Is : {account}
+            <div className="bg-light text-left p-5 m-5 shadow">
 
-                    <div className="row mt-4 p-2">
-                        <div className='col-2'>
-                            Network : <span className="text-danger text-capitalize" id='user_network'> {contractNetwork}</span>
-                        </div>
-                        <div className='col-5'>
-                            User : <span className='text-danger' id='user_address'> {account}</span>
-                        </div>
-                        <div className='col-5'>
-                            Address : <span className='text-danger' id='contract_address'> {contractAddress}</span>
-                        </div>
+                <FormControlLabel
+                    sx={{display: 'block'}}
+                    control={
+                        <Switch checked={loading} onChange={() =>getTransactionAndTransferData(serverName,document.getElementById('transaction_text').textContent) && setLoading(!loading)} name="loading" color="primary" disabled={loading && serverName=='production' ? true : false}/>
+                    }
+                    label={<span style={{fontWeight: 'bold',color:'black'}} id='transaction_text'>{loading && serverName=='production' ? "Transactions" : (!loading && serverName=='local' ? 'Transactions' : 'Transfer' )}</span>}
+                />
+                
+                <hr/>
+
+
+                Alaaa : {userTransactionAndTransfer.length}
+                Current Account Is : {account}
+
+
+                Transactions List - {userTransactionAndTransfer.length}
+
+                <div className="row mt-4 p-2">
+                    <div className='col-4'>
+                        Network : <span className="text-danger text-capitalize" id='user_network'> {contractNetwork}</span>
                     </div>
+                    <div className='col-4'>
+                        User : <span className='text-danger' id='user_address'> {account}</span>
+                    </div>
+                    <div className='col-4'>
+                        Contract Address : <span className='text-danger' id='contract_address'> {contractAddress}</span>
+                    </div>
+                </div>
 
-                    <hr/>
+                <hr/>
 
-                    {/*  Current Account Is : {account} */}
+                {/*  Current Account Is : {account} */}
 
-                    {
-                        loading && serverName=='production' ? (
-                        <>
-                            <p>Transaction Prod</p>
+                {
+                    loading && serverName=='production' ? (
+                    <>
+                        <p className='text-warning'>Transaction Prod</p>
+
+                        <table class="table table-secondary table-striped mt-5">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Token Name</th>
+                                    <th scope="col">Token Symbol</th>
+                                    <th scope="col">From Address</th>
+                                    <th scope="col">To Address</th>
+                                    <th scope="col">Network</th>
+                                    <th scope="col">TimeStamp</th>
+                                    <th scope="col">Value</th>
+                                    <th scope="col">Success</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    currentItems.length > 0 ? (
+                                        currentItems.map((transaction, index) => (
+                                            <tr>
+                                                <th scope="row">{transaction.token_name}</th>
+                                                <td>{transaction.token_symbol}</td>
+                                                <td>{transaction.transaction_from}</td>
+                                                <td>{transaction.transaction_to}</td>
+                                                <td>{transaction.network}</td>
+                                                <td>{new Date(transaction.created).toLocaleDateString('en-US',options)}</td>
+                                                <td>{transaction.transaction_amount}</td>
+                                                <td>{String(transaction.is_succsess) == 'true' ? 'Completed' : 'Failed'}</td>
+                                            </tr>
+                                        )) 
+                                    )
+                                    :
+                                    (
+                                        <th scope="col-12">Not found any transaction history for your account</th>
+                                    )
+                                }
+                            </tbody>
+                        </table>
+                        <ReactPaginate
+                            pageCount={pageCount} 
+                            pageRangeDisplayed={5} 
+                            marginPagesDisplayed={2} 
+                            onPageChange={handlePageClick} 
+                            containerClassName={'pagination'} 
+                            activeClassName={'active'} 
+                            disabled={true}
+                        />
+                    </>
+                    )
+                    :
+                    (
+                        !loading && serverName=='local' ? (
+                            <>
+                                <p className="text-warning">Transactions Local</p>
+
+                                <table class="table table-secondary table-striped mt-5">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Token Name</th>
+                                            <th scope="col">Token Symbol</th>
+                                            <th scope="col">From Address</th>
+                                            <th scope="col">To Address</th>
+                                            <th scope="col">Network</th>
+                                            <th scope="col">TimeStamp</th>
+                                            <th scope="col">Value</th>
+                                            <th scope="col">Success</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            currentItems.map((transaction, index) => (
+                                                <tr>
+                                                    <th scope="row">{transaction.token_name}</th>
+                                                    <td>{transaction.token_symbol}</td>
+                                                    <td>{transaction.transaction_from}</td>
+                                                    <td>{transaction.transaction_to}</td>
+                                                    <td>{transaction.network}</td>
+                                                    <td>{new Date(transaction.created).toLocaleDateString('en-US',options)}</td>
+                                                    <td>{transaction.transaction_amount}</td>
+                                                    <td>{String(transaction.is_succsess) == 'true' ? 'Completed' : 'Failed'}</td>
+                                                </tr>
+                                            )) 
+                                        }
+                                    </tbody>
+                                </table>
                             
-                            <ReactPaginate
-                                pageCount={pageCount} 
-                                pageRangeDisplayed={5} 
-                                marginPagesDisplayed={2} 
-                                onPageChange={handlePageClick} 
-                                containerClassName={'pagination'} 
-                                activeClassName={'active'} 
-                                disabled={true}
-                            />
-                        </>
+                            </>
                         )
                         :
                         (
-                            !loading && serverName=='local' ? (
-                                <p>Transactions Local</p>
-                            )
-                            :
-                            (
-                                <p>Transfer Local</p>
-                            )
+                            <p className="text-warning">Transfer Local</p>
                         )
-                    }
+                    )
+                }
 
-                    
-                </div>
-
+            
             </div>
+
+
         </>
     )
 }
